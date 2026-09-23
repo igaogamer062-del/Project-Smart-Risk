3dc3eac26ef072a787d6dd606c3b0411956a3b8d
Ygor Del Rio Lourenco <Ygor939@gmail.com>
2026-09-23 16:39:11 -0300

    Conecta o Smart Risk ao Supabase

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 /* Configuração pública do SmartRisk. Use somente a chave publishable/anon. */
 window.SMART_RISK_CONFIG = Object.freeze({
-  supabaseUrl: "COLE_AQUI_A_URL_DO_SUPABASE",
-  supabasePublishableKey: "COLE_AQUI_A_CHAVE_PUBLISHABLE_DO_SUPABASE",
+  supabaseUrl: "https://zeswbeivbxayksihitfv.supabase.co",
+  supabasePublishableKey: "sb_publishable_FVXKCr9DqG2b9CEpuedseA_x7JjdZlR",
   sessionMaxHours: 13
 });
